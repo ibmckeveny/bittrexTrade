071124759973738ddce92207882042356e02226a
wreckednerd <ibmckeveny@gmail.com>
2018-05-27 19:50:53 -0400

    api working

--- bittrexTrade/driver.py
+++ bittrexTrade/driver.py
@@ -1,7 +1,6 @@
 from bittrex.bittrex import Bittrex, API_V1_1
-my_bittrex = Bittrex(None, None, api_version=API_V1_1)  # or defaulting to v1.1 as Bittrex(None, None)
+my_bittrex = Bittrex( api_version=API_V1_1)  # or defaulting to v1.1 as Bittrex(None, None)
 
 
 print(my_bittrex.get_markets())
-
-xyzzyx
+print(my_bittrex.get_balance('ETH'))
